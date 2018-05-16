2404613fdc3d193c26242dc7d835306ffbc076ff
Jürgen Dufner <juergen@jdufner.de>
2018-05-16 23:33:11 +0200

    update copyright

--- solver/src/test/java/de/jdufner/sudoku/solver/ddt/sudoku_fuer_profis/SudokuFuerProfisTest.java
+++ solver/src/test/java/de/jdufner/sudoku/solver/ddt/sudoku_fuer_profis/SudokuFuerProfisTest.java
@@ -1,28 +1,23 @@
-// $Id$
-
 /*
- * Gudoku (http://sourceforge.net/projects/gudoku)
- * Sudoku-Implementierung auf Basis des Google Webtoolkit
- * (http://code.google.com/webtoolkit/). Die Lösungsalgorithmen in Java laufen
- * parallel. Die Sudoku-Rätsel werden mittels JDBC in einer Datenbank
- * gespeichert.
+ * Sudoku is a puzzle game. It solves and generates puzzles in different
+ * formats.
+ * Copyright (C) 2008-2018  Juergen Dufner
  *
- * Copyright (C) 2008 Jürgen Dufner
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * Dieses Programm ist freie Software. Sie können es unter den Bedingungen der
- * GNU General Public License, wie von der Free Software Foundation
- * veröffentlicht, weitergeben und/oder modifizieren, entweder gemäß Version 3
- * der Lizenz oder (nach Ihrer Option) jeder späteren Version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * Die Veröffentlichung dieses Programms erfolgt in der Hoffnung, daß es Ihnen
- * von Nutzen sein wird, aber OHNE IRGENDEINE GARANTIE, sogar ohne die
- * implizite Garantie der MARKTREIFE oder der VERWENDBARKEIT FÜR EINEN
- * BESTIMMTEN ZWECK. Details finden Sie in der GNU General Public License.
- *
- * Sie sollten ein Exemplar der GNU General Public License zusammen mit diesem
- * Programm erhalten haben. Falls nicht, siehe <http://www.gnu.org/licenses/>.
- *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+// $Id$
 package de.jdufner.sudoku.solver.ddt.sudoku_fuer_profis;
 
 import static org.junit.Assert.assertEquals;
